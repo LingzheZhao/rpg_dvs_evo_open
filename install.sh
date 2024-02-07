54ab5cb8e8357b9b1917f56c40d871628b23117a
Lingzhe Zhao <zhaolingzhe@westlake.edu.cn>
2024-02-07 16:26:09 +0800

    change dependencies git URL from ssh to http; update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,36 @@
 # #!/bin/bash
 
 echo "First, we install the required apt packages"
-sudo add-apt-repository ppa:inivation-ppa/inivation
+
+# Note: this ppa contains OpenCV 4 that may break ROS melodic, so don't use it
+# sudo add-apt-repository ppa:inivation-ppa/inivation-bionic
+
 sudo apt-get update
-sudo apt-get install            \
-    ros-$1-sophus               \
-    ros-$1-pcl-ros              \
-    ros-$1-eigen-conversions    \
-    ros-$1-camera-info-manager  \
-    ros-$1-image-view           \
-    libcaer-dev                 \
-    libfftw3-dev libfftw3-doc   \
-    libglew-dev                 \
-    libopencv-dev               \
-    libyaml-cpp-dev             \
-    python-catkin-tools         \
-    ros-$1-camera-info-manager  \
-    ros-$1-image-geometry
+sudo apt-get install \
+    ros-melodic-sophus \
+    ros-melodic-pcl-ros \
+    ros-melodic-eigen-conversions \
+    ros-melodic-camera-info-manager \
+    ros-melodic-image-view \
+    ros-melodic-camera-info-manager \
+    ros-melodic-image-geometry \
+    libfftw3-dev libfftw3-doc \
+    libglew-dev \
+    libopencv-dev \
+    libyaml-cpp-dev \
+    python-catkin-tools \
+    python-rosdep \
+    python-vcstool
 
-
+# Build libcaer
+git clone --recursive https://gitlab.com/inivation/dv/libcaer
+cd libcaer
+mkdir build
+cd build
+cmake ..
+make -j
+sudo make install
+cd ../../
 
 echo "Second, we clone the evo dependencies"
 vcs-import < rpg_dvs_evo_open/dependencies.yaml
